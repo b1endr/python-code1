a734c97958488d8e61a7639ebd7e90aafc0d25a3
Jakub Chudecki <jjchudecki@gmail.com>
2024-03-14 20:02:51 +0000

    added second argument

--- function1.py
+++ function1.py
@@ -1,6 +1,10 @@
-def my_function (n):
+
+
+def my_function (n,c):
     
     for num in range(n):
         print(1 + num)
+    for num in range(c):
+        print(1 + num)
 
-my_function(11)
+my_function(10,12)
